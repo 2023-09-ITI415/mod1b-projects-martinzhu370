8a5c82ba34b999705391e7c7875b146a720460b2
martinzhu370 <martinzhu370@gmail.com>
2023-09-28 18:08:24 -0400

    trying

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -9,8 +9,8 @@ public class AppleTree : MonoBehaviour
     public GameObject applePrefab;
     public float speed = 1f;
     public float leftAndRightEdge = 10f;
-    public float changeToChangeDirection;
-    public float secondsBetweenAppleDrops; 
+    public float changeToChangeDirection = 0.1f;
+    public float secondsBetweenAppleDrops = 1f; 
 
 
 
@@ -18,13 +18,14 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+        Invoke("DropApple", 2f);
     }
 
     void DropApple() {
 
         GameObject apple = Instantiate(applePrefab) as GameObject;
         apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
     }    
 
 
--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
 
     }
 
     // Update is called once per frame
     void Update()
     {
-                // Get the current screen position of the mouse from Input
+        // Get the current screen position of the mouse from Input
         Vector3 mousePos2D = Input.mousePosition; // 1
         // The Camera's z position sets the how far to push the mouse into 3D
         mousePos2D.z = -Camera.main.transform.position.z; // 2
@@ -24,6 +30,22 @@ public class Basket : MonoBehaviour
         Vector3 pos = this.transform.position;
         pos.x = mousePos3D.x;
         this.transform.position = pos;
+
+    }
+
+    private void OnCollisionEnter(Collision coll){
+        GameObject collidedWith = coll.gameObject;
+        if(collidedWith.tag == "Apple") {
+            Destroy(collidedWith);
+        
+            int score = int.Parse(scoreGT.text);
+            score += 1;
+            scoreGT.text = score.ToString();
+
+        if (score > HighScore.score){
+            HighScore.score = score;
+        }
+        }
 
     }
 }
