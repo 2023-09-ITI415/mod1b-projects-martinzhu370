aa581e5a2840a8d61bba1b96981685801c0c06a6
martinzhu370 <martinzhu370@gmail.com>
2023-09-28 20:41:16 -0400

    primary setup

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+public class Slingshot : MonoBehaviour {
+    public GameObject launchPoint;
+    void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive( false );
+    }
+
+    void OnMouseEnter() {
+        launchPoint.SetActive( true );
+    }
+    void OnMouseExit() {
+        launchPoint.SetActive( false );
+
+    }
+}
